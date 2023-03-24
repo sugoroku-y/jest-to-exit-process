ce6907aa298893962a59adbfd78794c8fcd9f2de
YEBISUYA Sugoroku <yebisuya@gmail.com>
2023-03-25 06:31:15 +0900

    fix: テスト失敗時のメッセージをJEST標準のマッチャーに似せたものに変更

--- src/index.ts
+++ src/index.ts
@@ -9,43 +9,66 @@ class ProcessExitContext {
 
   private checked = false;
 
-  constructor(private readonly code?: number) {}
+  constructor(
+    private readonly context: jest.MatcherContext,
+    private readonly code?: number,
+  ) {}
 
   check(state?: { ex: unknown }): jest.CustomMatcherResult {
     this.checked = true;
-    let pass = false;
-    let message = '';
-    BLOCK: {
-      if (this.process_exit.mock.calls.length > 0) {
-        // process.exitが1度でも呼ばれた
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const actual = this.process_exit.mock.calls[0]![0] ?? 0;
-        if (this.code === undefined) {
-          // 期待する終了コードが指定されていなければ、終了コードが何であっても成功
-          pass = true;
-          message = `Received function exit process, but not expected.`;
-          break BLOCK;
-        }
-        // 期待する終了コードが指定されていれば、実際の終了コードと比較
-        if (actual !== this.code) {
-          // 期待する終了コードと一致しない -> 失敗
-          message = `Excepted process exit code: ${this.code}\nReceived process exit code: ${actual}`;
-          break BLOCK;
-        }
-        // 期待する終了コードと一致 -> 成功
-        pass = true;
-        message = `Received function exit process with ${actual}`;
-        break BLOCK;
-      }
+    const { isNot, promise, utils } = this.context;
+    const code = this.code;
+    const message = (...messages: string[]): string =>
+      [
+        utils.matcherHint(
+          'toExitProcess',
+          undefined,
+          code === undefined ? '' : undefined,
+          {
+            ...(isNot && { isNot }),
+            ...(promise && { promise }),
+          },
+        ),
+        '',
+        ...messages,
+      ].join('\n');
+    // 最初のprocess.exit呼び出し
+    const [call] = this.process_exit.mock.calls;
+    if (!call) {
+      // process.exitが1度も呼ばれなかった
       if (state) {
-        // process.exitが1度も呼ばれず、例外が投げられた -> 例外をそのまま投げなおす
+        // process.exitが呼ばれる前に例外が投げられた -> 例外をそのまま投げなおす
         throw state.ex;
       }
       // process.exitが1度も呼ばれない場合
-      message = `Received function did not exit process.`;
-      break BLOCK;
+      return {
+        pass: false,
+        message: () => message('Received function did not exit process'),
+      };
     }
-    return { pass, message: () => message };
+    // 指定された終了コードを取得
+    const actual = call[0] ?? 0;
+    if (this.code === undefined) {
+      // 期待する終了コードが指定されていなければ、終了コードが何であっても成功
+      return {
+        pass: true,
+        message: () =>
+          message(`Exit code: ${utils.RECEIVED_COLOR(`${actual}`)}`),
+      };
+    }
+    // 期待する終了コードが指定されていれば、実際の終了コードと比較
+    return {
+      pass: actual === this.code,
+      message: () =>
+        message(
+          `Expected exit code:${isNot ? ' not' : ''} ${utils.EXPECTED_COLOR(
+            `${code}`,
+          )}`,
+          `Received exit code:${isNot ? '    ' : ''} ${utils.RECEIVED_COLOR(
+            `${actual}`,
+          )}`,
+        ),
+    };
   }
 
   dispose() {
@@ -60,7 +83,7 @@ function toExitProcess(
   received: () => unknown,
   code?: number,
 ): jest.CustomMatcherResult | Promise<jest.CustomMatcherResult> {
-  const context = new ProcessExitContext(code);
+  const context = new ProcessExitContext(this, code);
   try {
     const result = received();
     if (result instanceof Promise) {
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,135 +1,270 @@
+import assert from 'assert';
 import '../src';
 
 const EXIT_MAP = {
+  // process.exitを呼ばない
   'does not exit': null,
+  // process.exitを終了コード無しで呼ぶ
   'exit without code': [],
+  // process.exitを終了コード0で呼ぶ
   'exit with code: 0': [0],
+  // process.exitを終了コード-1で呼ぶ
   'exit with code:-1': [-1],
 } as const;
 
 const EXPECTED_MAP = {
+  // toExitProcessへ終了コードの指定なし
   'without code': [],
+  // toExitProcessへ終了コード0を指定
   'with code: 0': [0],
+  // toExitProcessへ終了コード-1を指定
   'with code:-1': [-1],
 } as const;
 
+const PASS_OR_FAIL: Record<
+  keyof typeof EXIT_MAP,
+  Record<keyof typeof EXPECTED_MAP, 'pass' | 'fail'>
+> = {
+  // process.exitが呼ばれなかったら失敗
+  'does not exit': {
+    'without code': 'fail',
+    'with code: 0': 'fail',
+    'with code:-1': 'fail',
+  },
+  // process.exitが終了コード無しで呼ばれたら、0を指定して呼ばれたときと同じ
+  'exit without code': {
+    'without code': 'pass',
+    'with code: 0': 'pass',
+    'with code:-1': 'fail',
+  },
+  // process.exitが終了コード0で呼ばれたら、
+  'exit with code: 0': {
+    // toExitProcessに終了コードが指定されていなければ成功
+    'without code': 'pass',
+    // toExitProcessに指定された終了コードと一致すれば成功
+    'with code: 0': 'pass',
+    // toExitProcessに指定された終了コードと一致しなければ失敗
+    'with code:-1': 'fail',
+  },
+  // process.exitが終了コード-1で呼ばれたら、
+  'exit with code:-1': {
+    // toExitProcessに終了コードが指定されていなければ成功
+    'without code': 'pass',
+    // toExitProcessに指定された終了コードと一致しなければ失敗
+    'with code: 0': 'fail',
+    // toExitProcessに指定された終了コードと一致すれば成功
+    'with code:-1': 'pass',
+  },
+} as const;
+
+// eslint-disable-next-line no-control-regex
+const ESCAPE_SEQUENCE = /(?:\x1b\[\d+m)*/;
+
 describe('.toExitProcess', () => {
-  test.each`
-    exit                   | expected          | pass
-    ${'does not exit'}     | ${'without code'} | ${false}
-    ${'does not exit'}     | ${'with code: 0'} | ${false}
-    ${'does not exit'}     | ${'with code:-1'} | ${false}
-    ${'exit without code'} | ${'without code'} | ${true}
-    ${'exit without code'} | ${'with code: 0'} | ${true}
-    ${'exit without code'} | ${'with code:-1'} | ${false}
-    ${'exit with code: 0'} | ${'without code'} | ${true}
-    ${'exit with code: 0'} | ${'with code: 0'} | ${true}
-    ${'exit with code: 0'} | ${'with code:-1'} | ${false}
-    ${'exit with code:-1'} | ${'without code'} | ${true}
-    ${'exit with code:-1'} | ${'with code: 0'} | ${false}
-    ${'exit with code:-1'} | ${'with code:-1'} | ${true}
-  `(
-    'process $exit, expected to exit $expected: $pass',
-    async ({
-      exit,
-      expected,
-      pass,
-    }: {
-      exit: keyof typeof EXIT_MAP;
-      expected: keyof typeof EXPECTED_MAP;
-      pass: boolean;
-    }) => {
-      const exitParam = EXIT_MAP[exit];
-      const toExitProcessParam = EXPECTED_MAP[expected];
-      const testProc = () => {
-        exitParam && process.exit(...exitParam);
-      };
-      const testProcAsync = async () => {
-        await new Promise(r => setTimeout(r, 1));
-        testProc();
-      };
-      if (pass) {
-        // .notが付かなければ成功となる組み合わせ
-        expect(() => {
+  describe.each`
+    exit
+    ${'does not exit'}
+    ${'exit without code'}
+    ${'exit with code: 0'}
+    ${'exit with code:-1'}
+  `('process $exit', ({ exit }: { exit: keyof typeof EXIT_MAP }) => {
+    describe.each`
+      expected
+      ${'without code'}
+      ${'with code: 0'}
+      ${'with code:-1'}
+    `(
+      'expected to exit $expected',
+      ({ expected }: { expected: keyof typeof EXPECTED_MAP }) => {
+        const exitParam = EXIT_MAP[exit];
+        const toExitProcessParam = EXPECTED_MAP[expected];
+        const testProc = exitParam
+          ? () => {
+              process.exit(...exitParam);
+            }
+          : () => undefined;
+        const testProcAsync = async () => {
+          await new Promise(r => setTimeout(r, 1));
           testProc();
-        }).toExitProcess(...toExitProcessParam);
-        // 非同期
-        await expect(async () => {
-          await testProcAsync();
-        }).toExitProcess(...toExitProcessParam);
-        // .notが付いてるのは失敗となる
-        const snapshot = `"Received function exit process${
-          toExitProcessParam.length
-            ? ` with ${toExitProcessParam[0]}`
-            : ', but not expected.'
-        }"`;
-        expect(() => {
-          expect(() => {
-            testProc();
-          }).not.toExitProcess(...toExitProcessParam);
-        }).toThrowErrorMatchingInlineSnapshot(snapshot);
-        // 非同期
-        await expect(async () => {
-          await expect(async () => {
-            await testProcAsync();
-          }).not.toExitProcess(...toExitProcessParam);
-        }).rejects.toThrowErrorMatchingInlineSnapshot(snapshot);
-      } else {
-        // .notが付かなければ失敗となる組み合わせ
-        const snapshot = !exitParam
-          ? `"Received function did not exit process."`
-          : !toExitProcessParam.length
-          ? `"Received function exit process, but not expected."`
-          : `
-"Excepted process exit code: ${toExitProcessParam[0]}
-Received process exit code: ${exitParam[0] ?? 0}"
-`;
-        expect(() => {
-          expect(() => {
-            testProc();
-          }).toExitProcess(...toExitProcessParam);
-        }).toThrowErrorMatchingInlineSnapshot(snapshot);
-        // 非同期
-        await expect(async () => {
-          await expect(async () => {
-            await testProcAsync();
-          }).toExitProcess(...toExitProcessParam);
-        }).rejects.toThrowErrorMatchingInlineSnapshot(snapshot);
-        // .notが付いてるので成功
-        expect(() => {
-          testProc();
-        }).not.toExitProcess(...toExitProcessParam);
-        // 非同期
-        await expect(async () => {
-          await testProcAsync();
-        }).not.toExitProcess(...toExitProcessParam);
-      }
-    },
-  );
+        };
+        switch (PASS_OR_FAIL[exit][expected]) {
+          // .notが付かなければ成功となる組み合わせ
+          case 'pass':
+            {
+              test('without .not: pass', () => {
+                expect(() => {
+                  testProc();
+                }).toExitProcess(...toExitProcessParam);
+              });
+              // 非同期
+              test('without .not async: pass', async () => {
+                await expect(async () => {
+                  await testProcAsync();
+                }).toExitProcess(...toExitProcessParam);
+              });
+              assert(exitParam, 'exitParamがnullのパターンにpassはない');
+              const pattern = toExitProcessParam.length
+                ? regexp`^
+                  ${ESCAPE_SEQUENCE}expect\(${ESCAPE_SEQUENCE}received${ESCAPE_SEQUENCE}\)\.${ESCAPE_SEQUENCE}not${ESCAPE_SEQUENCE}\.${ESCAPE_SEQUENCE}toExitProcess${ESCAPE_SEQUENCE}\(${ESCAPE_SEQUENCE}expected${ESCAPE_SEQUENCE}\)${ESCAPE_SEQUENCE}\
+                  \
+                  Expected exit code: not ${ESCAPE_SEQUENCE}${
+                    toExitProcessParam[0]
+                  }${ESCAPE_SEQUENCE}\
+                  Received exit code: +${ESCAPE_SEQUENCE}${
+                    exitParam[0] ?? 0
+                  }${ESCAPE_SEQUENCE}
+                  $`
+                : regexp`^
+                  ${ESCAPE_SEQUENCE}expect\(${ESCAPE_SEQUENCE}received${ESCAPE_SEQUENCE}\)\.${ESCAPE_SEQUENCE}not${ESCAPE_SEQUENCE}\.${ESCAPE_SEQUENCE}toExitProcess${ESCAPE_SEQUENCE}\(${ESCAPE_SEQUENCE}\)${ESCAPE_SEQUENCE}\
+                  \
+                  Exit code: ${ESCAPE_SEQUENCE}${
+                    exitParam[0] ?? 0
+                  }${ESCAPE_SEQUENCE}
+                  $`;
+              // .notが付いていれば失敗
+              test('with .not: fail', () => {
+                expect(() => {
+                  expect(() => {
+                    testProc();
+                  }).not.toExitProcess(...toExitProcessParam);
+                }).toThrow(pattern);
+              });
+              // 非同期
+              test('with .not async: fail', async () => {
+                await expect(async () => {
+                  await expect(async () => {
+                    await testProcAsync();
+                  }).not.toExitProcess(...toExitProcessParam);
+                }).rejects.toThrow(pattern);
+              });
+            }
+            break;
+          // .notが付かなければ失敗となる組み合わせ
+          case 'fail':
+            {
+              const pattern = !exitParam
+                ? regexp`^
+                  ${ESCAPE_SEQUENCE}expect\(${ESCAPE_SEQUENCE}received${ESCAPE_SEQUENCE}\)\.${ESCAPE_SEQUENCE}toExitProcess${ESCAPE_SEQUENCE}\(${ESCAPE_SEQUENCE}${
+                    toExitProcessParam.length
+                      ? regexp`expected${ESCAPE_SEQUENCE}`
+                      : ''
+                  }\)${ESCAPE_SEQUENCE}\
+                  \
+                  Received function did not exit process
+                  $`
+                : regexp`^
+                  ${ESCAPE_SEQUENCE}expect\(${ESCAPE_SEQUENCE}received${ESCAPE_SEQUENCE}\)\.${ESCAPE_SEQUENCE}toExitProcess${ESCAPE_SEQUENCE}\(${ESCAPE_SEQUENCE}expected${ESCAPE_SEQUENCE}\)${ESCAPE_SEQUENCE}\
+                  \
+                  Expected exit code: +${ESCAPE_SEQUENCE}${
+                    // exitParamがnullでなくてtoExitProcessParamが空のパターンに'fail'はない
+                    toExitProcessParam[0] ?? 0
+                  }${ESCAPE_SEQUENCE}\
+                  Received exit code: +${ESCAPE_SEQUENCE}${
+                    exitParam[0] ?? 0
+                  }${ESCAPE_SEQUENCE}
+                  $`;
+              test('without .not: fail', () => {
+                expect(() => {
+                  expect(() => {
+                    testProc();
+                  }).toExitProcess(...toExitProcessParam);
+                }).toThrow(pattern);
+              });
+              // 非同期
+              test('without .not async: fail', async () => {
+                await expect(async () => {
+                  await expect(async () => {
+                    await testProcAsync();
+                  }).toExitProcess(...toExitProcessParam);
+                }).rejects.toThrow(pattern);
+              });
+              // .notが付いていれば成功
+              test('with .not: pass', () => {
+                expect(() => {
+                  testProc();
+                }).not.toExitProcess(...toExitProcessParam);
+              });
+              // 非同期
+              test('with .not async: pass', async () => {
+                await expect(async () => {
+                  await testProcAsync();
+                }).not.toExitProcess(...toExitProcessParam);
+              });
+            }
+            break;
+        }
+      },
+    );
+  });
 
   // プロセス終了前に例外が出た場合は失敗と見なす
-  test('exception in .toExitProcess', async () => {
+  describe('exception thrown', () => {
+    class TestException extends Error {}
+    class AsyncTestException extends Error {}
     // 同期
     const testProc = (): void => {
-      throw new Error('normal');
+      throw new TestException();
     };
     // 非同期
     const testProcAsync = async (): Promise<void> => {
       await new Promise(r => setTimeout(r, 1));
-      throw new Error('async');
+      throw new AsyncTestException();
     };
-    expect(() => {
-      expect(testProc).toExitProcess();
-    }).toThrowErrorMatchingInlineSnapshot(`"normal"`);
-    await expect(async () => {
-      await expect(testProcAsync).toExitProcess();
-    }).rejects.toThrowErrorMatchingInlineSnapshot(`"async"`);
+    test('without .not: fail', () => {
+      expect(() => {
+        expect(testProc).toExitProcess();
+      }).toThrow(TestException);
+    });
+    test('without .not async: fail', async () => {
+      await expect(async () => {
+        await expect(testProcAsync).toExitProcess();
+      }).rejects.toThrow(AsyncTestException);
+    });
     // .notが付いてても例外の対応は同じ
-    expect(() => {
-      expect(testProc).not.toExitProcess();
-    }).toThrowErrorMatchingInlineSnapshot(`"normal"`);
-    await expect(async () => {
-      await expect(testProcAsync).not.toExitProcess();
-    }).rejects.toThrowErrorMatchingInlineSnapshot(`"async"`);
+    test('with .not: fail', () => {
+      expect(() => {
+        expect(testProc).not.toExitProcess();
+      }).toThrow(TestException);
+    });
+    test('with .not async: fail', async () => {
+      await expect(async () => {
+        await expect(testProcAsync).not.toExitProcess();
+      }).rejects.toThrow(AsyncTestException);
+    });
   });
 });
+
+function regexp(
+  template: TemplateStringsArray,
+  ...values: (RegExp | number | string | { flags: string })[]
+): RegExp {
+  let flags = '';
+  const pattern = template.raw
+    .map(s =>
+      s.replace(/(\\(?:.|\n)|(?<!\s) (?!\s))|\/\/.*|\/\*.*?\*\/|\s+/g, '$1'),
+    )
+    .reduce((r, e, i) => {
+      const value = values[i - 1];
+      if (
+        typeof value === 'object' &&
+        'flags' in value &&
+        typeof value.flags === 'string'
+      ) {
+        for (const flag of flags) {
+          if (!flags.includes(flag)) {
+            flags += flag;
+          }
+        }
+      }
+      const p =
+        value instanceof RegExp
+          ? value.source
+          : typeof value === 'number'
+          ? String(value)
+          : typeof value === 'string'
+          ? value.replace(/[.^$|()[\]{}+*?]/g, '\\$&')
+          : '';
+      return r.concat(p, e);
+    });
+  return new RegExp(pattern, flags);
+}
